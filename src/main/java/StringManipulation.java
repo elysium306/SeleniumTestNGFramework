8c0e442446ee5a03183eaf3f2482595ad5096ca2
elysium306 <omutwar@hotmail.com>
2023-08-23 23:32:52 -0500

    Added new file

---
+++ src/main/java/StringManipulation.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class StringManipulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String myName = "John Doe";
+		System.out.println(myName.toUpperCase());
+
+	}
+
+}
